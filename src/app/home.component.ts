9b1ad1e58bf83d810fad5093e62aa5181653c1be
fasidOnBitBucket <kader.mohideen2011@vit.ac.in>
2017-12-02 18:29:46 +0530

    Setup for Heroku deployment and Fixed some issue in query filters.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,6 +3,7 @@ import {SearchService} from './search.service';
 import {Subject} from 'rxjs/Subject';
 import { Chart } from 'angular-highcharts';
 import async from 'async';
+import { MatSnackBar } from '@angular/material';
 // import { Highcharts } from 'highcharts';
 var Highcharts = require('highcharts');
 
@@ -17,7 +18,8 @@ export class HomeComponent {
 	results: Object;
 	searchTerms = new Subject<string>();
 	subSearchTerm = new Subject<string>();
-	constructor(private searchService : SearchService){}
+	loading = false;
+	constructor(private searchService : SearchService , public snackBar: MatSnackBar){}
 	query ={
 		search : '',
 		from : null,
@@ -25,30 +27,31 @@ export class HomeComponent {
 	};
 
 	post() {
+		this.loading = true;
 		let hitCount = [] , year = [] , mostCited = {};
 		this.query.from = (this.query.from) ? this.query.from : new Date('1970-01-01');
 		this.query.to = (this.query.to) ? this.query.to : new Date();
-		console.log(this.query);
-
-		// this.searchService.queryEPMC(this.query);
 		if(this.query.from.getFullYear() == this.query.to.getFullYear()){
-			this.searchService.searchEntries(this.query.search , `(%20FIRST_PDATE:[${this.query.from.getTime()}%20TO%20${this.query.to.getTime()}])`)
+			this.searchService.searchEntries(this.query.search , `( FIRST_PDATE:[${this.query.from.yyyymmdd()} TO ${this.query.to.yyyymmdd()}])`)
 				.subscribe(results => {
 					console.log(results)
 					hitCount.push(results.hitCount);
 					year.push(results.resultList.result[0].pubYear);
 					mostCited[results.resultList.result[0].pubYear] = results.resultList.result;
 					getHistogram({query : this.query.search , from : this.query.from , to : this.query.to ,mostCited : mostCited, hitCount : hitCount , year : year});
-					})
+					}, error => {
+											this.loading = false;
+											console.log('error' , error);
+											this.snackBar.open('Error in fetching data!!' , 'CLOSE')
+											})
 			} else {
 				var topRangeEnd = new Date(`${this.query.from.getFullYear()}-12-31`);
 				var botRangeStart = new Date(`${this.query.to.getFullYear()}-01-01`);
-				var topRange = `[${this.query.from.getTime()}%20TO%20${topRangeEnd.getTime()}]`;
-				var botRange = `[${botRangeStart.getTime()}%20TO%20${this.query.to.getTime()}]`;
-				this.searchService.searchEntries(this.query.search , `(%20FIRST_PDATE:${topRange})`)
+				var topRange = `[${this.query.from.yyyymmdd()} TO ${this.query.from.getFullYear()}-12-31]`;
+				var botRange = `[${this.query.to.getFullYear()}-01-01 TO ${this.query.to.yyyymmdd()}]`;
+				this.searchService.searchEntries(this.query.search , `( FIRST_PDATE:${topRange})`)
 					.subscribe(resultsTop => {
 						if(resultsTop.hitCount !=0) {
-							// code...
 						hitCount.push(resultsTop.hitCount);
 						year.push(resultsTop.resultList.result[0].pubYear);
 						mostCited[resultsTop.resultList.result[0].pubYear] = resultsTop.resultList.result;
@@ -59,7 +62,7 @@ export class HomeComponent {
 								return pubYearStart < this.query.to.getFullYear()
 							} ,
 							(callback) => {
-								this.searchService.searchEntries(this.query.search , `(%20PUB_YEAR:${pubYearStart})`)
+								this.searchService.searchEntries(this.query.search , `( PUB_YEAR:${pubYearStart})`)
 									.subscribe(resultsYr => {
 										if(resultsYr.hitCount!=0) {
 											// code...
@@ -68,15 +71,14 @@ export class HomeComponent {
 										mostCited[resultsYr.resultList.result[0].pubYear] = resultsYr.resultList.result;
 										}
 										pubYearStart++;
-										callback(undefined , 'done');
+										callback(undefined , pubYearStart);
 										} , error => {
 											console.log('error' , error)
+											this.snackBar.open('Error in fetching data!!' , 'CLOSE')
 											})
 							},
 							(err , n) => {
-								console.log('asd' , n)
-								console.log(botRangeStart , this.query.to)
-								this.searchService.searchEntries(this.query.search , `(%20FIRST_PDATE:${botRange})`)
+								this.searchService.searchEntries(this.query.search , `( FIRST_PDATE:${botRange})`)
 									.subscribe(resultsBot => {
 										if(resultsBot.hitCount!=0) {
 											// code...
@@ -84,14 +86,20 @@ export class HomeComponent {
 										year.push(resultsBot.resultList.result[0].pubYear);
 										mostCited[resultsBot.resultList.result[0].pubYear] = resultsBot.resultList.result;
 										}
+										console.log(hitCount , year)
+										this.loading = false;
 										getHistogram({query : this.query.search , from : this.query.from , to : this.query.to ,mostCited : mostCited, hitCount : hitCount , year : year});
 										} , error => {
-											console.log('error' , error)
+											this.loading = false;
+											console.log('error' , error);
+											this.snackBar.open('Error in fetching data!!' , 'CLOSE')
 											})
 							}
 							)
 						} , error => {
-							console.log('error' , error)
+							this.loading = false;
+							console.log('error' , error);
+							this.snackBar.open('Error in fetching data!!' , 'CLOSE')
 							})
 			}
 	}
@@ -112,7 +120,8 @@ function getHistogram(data){
       tooltip: {
         formatter: function () {
             return `
-				<span style="font-weight : bolder;">Most citedc${this.x} : ${data.mostCited[this.x][0].title}</span><br>
+            	<h2>${this.x}</h2><br>		
+				<span style="font-weight : bolder;">Most cited: ${data.mostCited[this.x][0].title}</span><br>
 				<span>Cited Count : ${data.mostCited[this.x][0].citedByCount}</span>
             `
         }
@@ -130,120 +139,14 @@ function getHistogram(data){
     }]
 });
 }
-// 		var results;
-// 		this.searchService.search(this.searchTerms)
-// 			.subscribe( results => {
-// 				if(typeof results.resultList !='undefined'){
 
-// 				this.results = results;
-// 				// dataByYear(this.results.resultList.result);
-				
+//Replicating toISOString()
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
 
-// 				var check = '';
-				
-// 				    	// this.searchService.search.bind(this);
-
-// 				    	// async.whilst.bind(this);
-// 			async.whilst(
-// 				    () => { console.log('resultsCheck' , check , results.nextCursorMark , results.resultList.result.length); return (check != results.nextCursorMark && results.resultList.result.length >=1000); },
-// 				    (callback) => {
-						
-// 				    	check = results.nextCursorMark;
-// 				    	console.log('check' , results.nextCursorMark)
-// 				    	this.subSearchTerm.next(results.request.query);
-// 				        this.searchService.searchEntries(results.request.query, `&cursorMark=${check}`)
-// 				        	.subscribe(subResult => {
-// 				        		console.log(subResult);
-// 				        		results.nextCursorMark = subResult.nextCursorMark;
-// 				        		results.request = subResult.request;
-// 				        		console.log('next length',subResult.resultList.result.length , results.resultList.result.length);
-// 				        		// results.resultList.result.concat(subResult.resultList.result);
-// 				        		 Array.prototype.push.apply(results.resultList.result,subResult.resultList.result);
-// 				        		console.log('coe da' ,results.resultList.result.length);
-// 				        		callback(undefined , results);
-// 				        		})
-// 				    },
-// 				     (err, n) => {
-// 				        // 5 seconds have passed, n = 5
-// 				        // this.results = n;
-// 				        console.log(err , n)
-// 				        console.log('done' , results);
-// 				        var data = dataByYear(results.resultList.result);
-
-// Highcharts.chart('container', {
-//     title: {
-//         text: 'For the Query : '+results.request.query
-//     },
-//     xAxis: [{
-//         title: { text: 'Years' } , categories: data.yAxis
-//     }],
-
-//     yAxis: [{
-//         title: { text: 'No of Publications' }
-//     }],
-//       tooltip: {
-//         formatter: function () {
-//             return `
-// 				<span style="font-weight : bolder;">Most citedc${this.x} : ${data.seriesYear[this.x][0].title}</span><br>
-// 				<span>Cited Count : ${data.seriesYear[this.x][0].citedByCount}</span>
-//             `
-//         }
-//     },
-
-//     series: [ {
-//         name: 'No of Publications',
-//         type: 'histogram',
-//         data: data.count,
-//         id: 's1',
-//         marker: {
-//             radius: 1.5
-//         },
-//         state : 'h1'
-//     }]
-// });
-// 				    }
-// 				);
-
-		
-// 				} else {
-// 					this.results = [];
-
-// 				}
-// 				});
-
-// 			function dataByYear(result){
-// 				var series = [] , yr = [] , maxCount=[];
-// 				var yearfilter = [];
-// 				var seriesJson = {};
-// 				for (var i = 0; i < result.length; i++) {
-// 					var eachResult = result[i]; var arr = [];
-// 					console.log(eachResult.pubYear)
-// 					if(yearfilter.indexOf(eachResult.pubYear)!=-1){
-// 						seriesJson[eachResult.pubYear].push(eachResult);
-// 					}else{
-// 						yearfilter.push(eachResult.pubYear)
-// 						arr.push(eachResult)
-// 						seriesJson[eachResult.pubYear] = arr;
-// 					}
-// 				}
-// 				console.log('deii' , seriesJson)
-// 				for (var key in seriesJson) {
-// 					// code...
-// 					seriesJson[key].sort((a , b) => b.citedByCount - a.citedByCount);
-// 					yr.push(key);
-// 					maxCount.push(seriesJson[key].length)
-// 					series.push(seriesJson[key]);
-
-					
-// 				}
-
-// 				console.log('The JSON',seriesJson)
-// 				return {
-// 					yAxis : yr,
-// 					series : series,
-// 					count : maxCount,
-// 					seriesYear : seriesJson
-// 				};
-// 			}
-// 	}
-// }
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('-');
+};
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -30,41 +30,15 @@ export class SearchService {
 		.switchMap(term => this.searchEntries(term));
 		}
 	} 
-// https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=lol(%20PUB_YEAR:2015)&format=json&sort=CITED%20desc&pageSize=1
-// https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=malaria(%20FIRST_PDATE:2003-11-05)&format=json&pageSize=1&sort_cited:y
+
 	searchEntries(term , filter?) {
-		console.log('hi da')
-		// filter = (filter) ? filter : '';
 		if(filter) {
-			// code...
+			console.log(this.baseUrl+this.queryUrl+term+filter+this.formatUrl)
 		return this.http.get(this.baseUrl+this.queryUrl+term+filter+this.formatUrl).map((res) => res.json());
 	
 		}else {
 			filter = '';
 		return this.http.get(this.baseUrl+this.queryUrl+term+this.formatUrl+filter).map(res => res.json())
 		}
-						// .map((res) => {
-						// 	console.log('Hi')
-
-						// 	res = res.json();
-						// 	var check = '', count=0;
-						// 	return whilst(
-						// 	    function() { return count < 5; },
-						// 	    function(callback) {
-						// 	        count++;
-						// 	        setTimeout(function() {
-						// 	            callback(null, count);
-						// 	        }, 1000);
-						// 	    },
-						// 	    function (err, n) {
-						// 	        // 5 seconds have passed, n = 5
-						// 	        console.log(n)
-						// 	        return res
-						// 	    }
-						// 	);
-
-
-
-						// 	});
 	}
 }	
